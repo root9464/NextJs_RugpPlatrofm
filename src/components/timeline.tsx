c39f0231c7164c307b538464c3a365ce8d296eac
iO <senpai9464@yandex.ru>
2025-03-05 03:35:17 +0300

    upd: restyling timerule

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -3,16 +3,18 @@ import { animate, motion, useMotionValue, useTransform } from 'motion/react';
 import { useRef } from 'react';
 
 const markers = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const offset = 16;
 
 export const Timeline = () => {
-  const containerRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const width = useMotionValue(620);
-  const leftX = useMotionValue(0);
-  const rightX = useMotionValue(620);
-  const highlightWidth = useTransform(() => rightX.get() - leftX.get());
-
-  const markerPositions = markers.map((marker) => (marker / markers[markers.length - 1]) * width.get());
+  const effectiveWidth = width.get() - 2 * offset;
+  const markerPositions = markers.map((marker) => offset + (marker / 100) * effectiveWidth);
   const minDistance = markerPositions[1] - markerPositions[0];
+
+  const leftX = useMotionValue(offset);
+  const rightX = useMotionValue(offset + effectiveWidth);
+  const highlightWidth = useTransform(() => rightX.get() - leftX.get());
 
   const snap = (x: number, isLeft: boolean, otherX: number) => {
     const possibleMarkers = isLeft
@@ -32,12 +34,14 @@ export const Timeline = () => {
     animate(rightX, snapped, { duration: 0.2 });
   };
 
+  const percentageTransform = (x: number) => `${Math.round(((x - offset) / effectiveWidth) * 100)}%`;
+
   return (
-    <div className='mx-auto flex w-full max-w-2xl flex-col p-4'>
+    <div className='mx-auto flex w-full max-w-2xl flex-col' style={{ padding: offset }}>
       <motion.div ref={containerRef} className='relative h-16 overflow-hidden rounded-lg border border-gray-200 bg-gray-50' style={{ width }}>
         <div className='absolute inset-0'>
           {markerPositions.map((pos, index) => (
-            <div key={markers[index]} className='absolute h-4 w-px bg-red-500' style={{ left: pos }}>
+            <div key={markers[index]} className='absolute top-1/2 h-4 w-px -translate-y-1/2 bg-red-500' style={{ left: pos }}>
               <span className='absolute -top-6 left-1/2 -translate-x-1/2 text-xs text-gray-600'>{markers[index]}</span>
             </div>
           ))}
@@ -47,7 +51,7 @@ export const Timeline = () => {
 
         <motion.div
           drag='x'
-          dragConstraints={{ left: 0, right: width.get() }}
+          dragConstraints={{ left: offset, right: offset + effectiveWidth }}
           dragElastic={0}
           onDragEnd={handleLeftDragEnd}
           style={{ x: leftX }}
@@ -57,7 +61,7 @@ export const Timeline = () => {
 
         <motion.div
           drag='x'
-          dragConstraints={{ left: 0, right: width.get() }}
+          dragConstraints={{ left: offset, right: offset + effectiveWidth }}
           dragElastic={0}
           onDragEnd={handleRightDragEnd}
           style={{ x: rightX }}
@@ -67,8 +71,8 @@ export const Timeline = () => {
       </motion.div>
 
       <div className='mt-2 flex justify-between text-sm text-gray-600'>
-        <motion.span>{useTransform(leftX, (x) => `${Math.round((x / width.get()) * 100)}%`)}</motion.span>
-        <motion.span>{useTransform(rightX, (x) => `${Math.round((x / width.get()) * 100)}%`)}</motion.span>
+        <motion.span>{useTransform(leftX, percentageTransform)}</motion.span>
+        <motion.span>{useTransform(rightX, percentageTransform)}</motion.span>
       </div>
     </div>
   );
